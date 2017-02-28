a764a372a06556022206cfa899bb6867a008494a
Jason Roffel <jasonroffel@hotmail.nl>
2017-02-28 14:28:00 +0100

    Wrong place

--- arrayCollector.js
+++ arrayCollector.js
@@ -68,9 +68,9 @@ function gatherArray(noCache, topCallback) {
 					process.stdout.write("Evaluation");
 					process.stdout.write((storage.getItemSync('animeArray').length > maxProcesses*2 && runningTemp == false).toString());
 					process.stdout.write("\n\nValues");
-					process.stdout.write(storage.getItemSync('animeArray').length, maxProcesses*2, runningTemp.toString());
+					process.stdout.write(storage.getItemSync('animeArray').length.toString(), (maxProcesses*2).toString(), runningTemp.toString());
 
-					if(storage.getItemSync('animeArray').length.toString() > (maxProcesses*2).toString() && runningTemp == false) {
+					if(storage.getItemSync('animeArray').length > maxProcesses*2 && runningTemp == false) {
 						runningTemp = true
 						process.stdout.write("Running downloader halfway");
 						spawnProcesses(function() {

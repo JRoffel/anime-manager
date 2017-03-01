9ea6e4e1e7ebb7bcdc7932901e787a1ec050e121
jason roffel <jasonroffel@hotmail.nl>
2017-03-01 23:27:46 +0100

    In-progress revamp indexed

--- arrayCollector.js
+++ arrayCollector.js
@@ -11,147 +11,59 @@ var request = require('request');
 var child_process = require('child_process');
 var storage = require('node-persist');
 var sleep = require('sleep');
+var ObservableArray = require('observable-array');
 
 Anime.setDelay(10000);
+var animeLink = process.argv[2];
 var basePath = process.argv[3];
 var maxProcesses = process.argv[4] || 5;
-var animeArray = [];
 var iterator = 0;
-var runningTemp = false;
-storage.initSync();
-
-if(storage.getItemSync('animeArray') != null && storage.getItemSync('animeArray') != undefined && storage.getItemSync('animeArray') != [] && storage.getItemSync('animeArray').length != 0) {
-	process.stdout.write("Found partial list, downloading...");
-	spawnProcesses(null, function() {
-		storage.clearSync();
-		storage.setItemSync('animeArray', []);
-		process.stdout.write("Finished partial list download!");
-		gatherArray(false, function() {
-			process.stdout.write("Finished cached list download!");
-			process.stdout.write("Starting noCache search!");
-			gatherArray(true, function() {
-				process.stdout.write("No more new episodes found!");
-				process.exit();
-			})
+var activeProcesses = 0;
+var animeArray = new ObservableArray();
+animeArray.on('change', function(ev) {
+	//TODO: Double check logic on if statement and process counter (Do this when awake)
+	if(activeProcesses < maxProcesses && animeArray.length != 0) {
+		activeProcesses++;
+		spawnDownloadProcess(animeArray.shift(), function(name, success) {
+			activeProcesses--;
 		});
-	});
-} else {
-	storage.setItemSync('animeArray', []);
-	process.stdout.write("No partial list found");
-	gatherArray(false, function() {
-		process.stdout.write("Finished cached list download!");
-		process.stdout.write("Starting noCache search!");
-		gatherArray(true, function() {
-			process.stdout.write("No more new episodes found!");
-			process.exit();
-		})
-	});
-}
+	}
+})
 
-function gatherArray(noCache, topCallback) {
-	cloudscraper.get(process.argv[2], function(err, res, body) {
-		if(err) {
-			process.exit(1);
-		} else {
-			const $ = cheerio.load(body);
-			const result = $(".trAnime td .aAnime").map((i, element) => ({
-				url: $(element).attr('href')
-			})).get();
+cloudscraper.get(animeLink.toString(), function(err, res, body) {
+	if(err) {
+		throw err;
+	} else {
+		const $ = cheerio.load(body);
+		const result = $(".trAnime td .aAnime").map((i, element) => ({
+			url: $(element).attr('href');
+		})).get();
 
-			async.eachSeries(result, function(url, callback) {
-				process.stdout.write("\nAdding http://kissanime.ru" + url.url + " to job list!");
-				Anime.fromUrl("http://kissanime.ru" + url.url).then(function(anime) {
-					dirName = fileSanitizer(anime.name);
-					if(!fs.existsSync(path.join(basePath, dirName))) {
-						fs.mkdirSync(path.join(basePath, dirName));
-					}
-
-					if(storage.getItemSync('animeArray').length > maxProcesses*2 && runningTemp == false) {
-						runningTemp = true
-						process.stdout.write("Running downloader halfway");
-						var tempArray = storage.getItemSync('animeArray');
-						storage.setItemSync('animeArray', []);
-
-						spawnProcesses(tempArray, function() {
-							process.stdout.write("Done running partial downloads...");
-
-							runningTemp = false;
-						});
-					}
-
-					if(fs.existsSync(path.join(basePath, dirName, dirName + " Episode 001.mp4")) && noCache == false) {
-						process.stdout.write("Skipping " + dirName + " for now, because episode 1 exists");
-						callback();
-					} else  {
-						async.eachSeries(anime.episodes, function(epi, cb) {
-							epi.fetch().then(function(episode) {
-								episodeName = fileSanitizer(episode.name);
-								if(!fs.existsSync(path.join(basePath, dirName, episodeName + ".mp4"))) {
-									process.stdout.write("Adding " + episodeName + " to downloadList");
-									var temp = storage.getItemSync('animeArray');
-									temp.push({
-										url: episode.video_links[0],
-										fileName: path.join(basePath, dirName, episodeName + ".mp4")
-									});
-									storage.setItemSync('animeArray', temp);
-									cb(null);
-								} else {
-									process.stdout.write("Not adding " + episodeName + " because it already exists!");
-									cb(null);
-								}
-							}, function(err) {
-								process.stdout.write("Could not establish connection to kissanime!");
-								cb(null);
-							});
-						}, function(err) {
-							callback(null);
-						})
-					}
-				})
-			}, function(err) {
-				process.stdout.write("Indexed all anime, waiting for download threads to free up")
-				while(runningTemp) {
-					sleep.sleep(10);
+		async.eachSeries(result, function(url, callback) {
+			process.stdout.write("Indexing episodes of http://kissanime.ru" +url.url+ " for downloadqueue");
+			Anime.fromUrl('http://kissanime.ru' + url.url).then(function(anime) {
+				dirName = fileSanitizer(anime.name);
+				
+				if(!fs.existsSync(path.join(basePath, dirName))) {
+					fs.mkdirSync(path.join(basePath, dirName));
 				}
-				process.stdout.write("Download threads free, spawning download processes now!");
-				spawnProcesses(null, function() {
-					storage.clearSync();
-					storage.setItemSync('animeArray', []);
-					process.stdout.write("Processed finished with " + iterator + " fails");
-					topCallback();
+
+				async.eachSeries(anime.episodes, function(epi, cb) {
+					epi.fetch().then(function(episode) {
+						episodeName = fileSanitizer(episode.name);
+						//TODO: Fill in path
+						var animeObject = {
+							url: episode.video_links[0].url,
+							path: 
+						}
+					});
 				});
 			});
-		}
-	});
-}
-
-function spawnProcesses(downloadArray, callback) {
-	if(downloadArray == null) {
-		downloadArray = storage.getItemSync('animeArray');
+		});
 	}
-	async.eachLimit(downloadArray, maxProcesses, function(animeObject, callback) {
-		if(!fs.existsSync(animeObject.fileName)) {
-			thread = child_process.spawn('node', ['singleDownloader.js', animeObject.url.url, animeObject.fileName]);
-			thread.stdout.on('data', (data) => {
-				process.stdout.write(data.toString());
-			});
+});
 
-			thread.stderr.on('data', (data) => {
-				process.stdout.write(data.toString());
-				iterator++;
-			});
+//TODO: Make function that implements singleDownloader.js with children
+function spawnDownloadProcess() {
 
-			thread.on('close', (code) => {
-				if(code == 0) {
-					callback();
-				} else if(code == 1) {
-					iterator++;
-				}
-			});			
-		} else {
-			callback();
-		}
-	}, function(err) {
-		callback();
-	});
 }
